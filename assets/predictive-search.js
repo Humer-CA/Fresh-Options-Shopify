f2bd8c773eadc4b277ce4d8c694abf045a3c6a02
Humer-CA <humeraltares@gmail.com>
2023-10-17 10:50:35 +0800

    new github

---
+++ assets/predictive-search.js
@@ -0,0 +1,67 @@
+class PredictiveSearch extends HTMLElement {
+  constructor() {
+    super();
+
+    this.input = this.querySelector('input[type="search"]');
+    this.predictiveSearchResults = this.querySelector('#predictive-search');
+
+    this.input.addEventListener('input', this.debounce((event) => {
+      this.onChange(event);
+    }, 300).bind(this));
+
+    this.input.addEventListener('blur', () => {
+      this.close();
+    });
+  }
+
+  onChange() {
+    const searchTerm = this.input.value.trim();
+
+    if (!searchTerm.length) {
+      this.close();
+      return;
+    }
+
+    this.getSearchResults(searchTerm);
+  }
+
+  getSearchResults(searchTerm) {
+    fetch(`/search/suggest?q=${searchTerm}&section_id=predictive-search`)
+      .then((response) => {
+        if (!response.ok) {
+          var error = new Error(response.status);
+          this.close();
+          throw error;
+        }
+
+        return response.text();
+      })
+      .then((text) => {
+        const resultsMarkup = new DOMParser().parseFromString(text, 'text/html').querySelector('#shopify-section-predictive-search').innerHTML;
+        this.predictiveSearchResults.innerHTML = resultsMarkup;
+        this.open();
+      })
+      .catch((error) => {
+        this.close();
+        throw error;
+      });
+  }
+
+  open() {
+    this.predictiveSearchResults.style.display = 'block';
+  }
+
+  close() {
+    this.predictiveSearchResults.style.display = 'none';
+  }
+
+  debounce(fn, wait) {
+    let t;
+    return (...args) => {
+      clearTimeout(t);
+      t = setTimeout(() => fn.apply(this, args), wait);
+    };
+  }
+}
+
+customElements.define('predictive-search', PredictiveSearch);
